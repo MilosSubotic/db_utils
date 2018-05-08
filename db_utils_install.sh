fc3957d47dc5447e90fee163ac4617ac0364de71
Milos Subotic <milos.subotic.sm@gmail.com>
2018-05-08 22:20:00 +0200

    SQLite browser building.

--- db_utils_install.sh
+++ db_utils_install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 sudo apt install python3 python3-bibtexparser python3-requests
+
+# DB Browser for SQLite
+git clone https://github.com/MilosSubotic/sqlitebrowser
+git checkout file_name_stuff
+sudo apt install build-essential cmake libqt4-dev libsqlite3-dev
+cmake .
+make
+sudo make install
