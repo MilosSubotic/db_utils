8dd0eaa1d150cfca376378915e658ff8ad2481dc
Milos Subotic <milos.subotic.sm@gmail.com>
2020-08-04 17:23:24 +0200

    Installing new lib for finding citations.

--- source/install/install.sh
+++ source/install/install.sh
@@ -1,15 +1,51 @@
 #!/bin/bash
-
+###############################################################################
 # Add to PATH.
-D="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" >/dev/null 2>&1 && pwd )"
-echo "export PATH=\$PATH:$D" >> ~/.profile
 
+I=`dirname "${BASH_SOURCE[0]}"`
+P="`cd "$I/.." >/dev/null 2>&1 && pwd`"
+echo "export PATH=\$PATH:$P" >> ~/.bashrc
 
+###############################################################################
+# Check is this Ubuntu.
+
+DIST=`lsb_release --id | sed 's/^Distributor ID:[\t ]*\(.*\)$/\1/'`
+if [[ "$DIST" != "Ubuntu" ]]
+then
+	echo "This install script is made for Ubuntu GNU/Linux distribution!"
+	echo "You need to change it a little for your distribution!"
+	exit 1
+fi
+
+R=`lsb_release --release`
+MAJOR=`echo $R | sed -n 's/^Release:[\t ]*\([0-9]\+\)\.\([0-9]\+\)$/\1/p'`
+
+###############################################################################
 # For scripts.
-sudo apt install python3 python3-bibtexparser python3-requests
 
-# DB Browser for SQLite
-sudo apt install git build-essential cmake libqt4-dev libsqlite3-dev
+if (( $MAJOR <= 16 ))
+then
+	# New python3.
+	sudo add-apt-repository -y ppa:deadsnakes/ppa
+	sudo apt update
+	sudo apt install -y python3.7
+	sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 5
+	sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 7
+	# pip
+	sudo apt install -y python3.7-distutils
+	wget https://bootstrap.pypa.io/get-pip.py
+	sudo python3 get-pip.py
+	rm get-pip.py
+else
+	sudo apt install -y python3
+fi
+sudo apt install -y python3-bibtexparser python3-requests
+sudo pip3 install scholarly
+
+###############################################################################
+# DB Browser for SQLite.
+
+sudo apt install -y git build-essential cmake libqt4-dev libsqlite3-dev
 git clone https://github.com/MilosSubotic/sqlitebrowser
 pushd sqlitebrowser
 git checkout file_name_stuff
@@ -17,3 +53,5 @@ cmake .
 make -j4
 sudo make install
 popd
+
+###############################################################################
